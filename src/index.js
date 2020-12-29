5d69132807cc53ae6c1314d2e1f06fab4edae388
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-12-29 17:31:10 -0600

    changed renderSquare in board to handle click

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//component is independent and reusable bit of code, does the same thing as JS functions but returns HTML via render
 class Square extends React.Component {
     //Using "stat" to rEmEmBeR that it gets clicked via constructor (like java)
-    //Super is used when defining constructor
+    //Super is used when defining constructor, it calls it
     constructor(props) {
         super(props);
         this.state = {
-            //Null used because it is not expecting a return value, and simply a marker?
+            //Null used because it is not expecting a return value, this is its starting state aka unmarked
             value: null,
         };
     }
@@ -24,8 +25,20 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+    //adding constructor and array for squares
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
     renderSquare(i) {
-        return <Square value={i}/>;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
@@ -33,6 +46,7 @@ class Board extends React.Component {
 
         return (
             <div>
+                {/*Board is being made via css borders*/}
                 <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
@@ -59,6 +73,7 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
+                    {/*What is this backslash at the end? Calling for the board tags and all children?*/}
                     <Board />
                 </div>
                 <div className="game-info">
